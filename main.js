da4cb9dcde8cd4a35d31ed90da683a35dcdeb3a1
lukutense <lucaslmartinez0@gmail.com>
2022-07-23 13:02:53 -0300

    form y algunos eventos y validaciones

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const icono = document.querySelector('.icono')
 const enlaces= document.querySelector('.enlaces')
+const botonContacto= document.querySelector('#boton-contacto')
+const formOculto= document.querySelector('.form-oculto')
+const cerrarBoton = document.querySelector('.cerrar-boton')
 
 const menuDesplegar = ()=>{
     icono.addEventListener('click', ()=>{
@@ -8,3 +11,20 @@ const menuDesplegar = ()=>{
 }
 
 menuDesplegar()
+
+const clickContacto = ()=>{
+    botonContacto.addEventListener('click', ()=>{
+        formOculto.classList.remove('form-oculto')
+    })
+}
+
+clickContacto()
+
+const clickCerrarForm = ()=>{
+    cerrarBoton.addEventListener('click', (e)=>{
+        e.preventDefault()
+        formOculto.classList.add('form-oculto')
+    })
+}
+
+clickCerrarForm()
